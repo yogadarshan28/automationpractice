60745b43205a3a8d9aa47b4b84164a2b419410eb
yogadarshan <yogadarshan@FH-MACMINI-0510.local>
2026-02-03 15:03:00 +0530

    java practice

---
+++ src/test/java/practice.java
@@ -0,0 +1,14 @@
+public class practice {
+    public static void main(String[] args) {
+        int a =0;
+        int b =1;
+
+        for (int i =a;i>=10;i++){
+            System.out.println(a + "");
+            int c = a+b;
+            a=b;
+            b=c;
+
+        }
+    }
+}
